9dd18938d6c438224928b7da865d2cb7cb20edce
dubey <you@example.com>
2021-03-28 01:19:31 +0530

    new changes in the application has been done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import {ChatEngine} from 'react-chat-engine';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
+import './App.css';
 
-const App=()=>
-{
-    if(!localStorage.getItem('username'))
-    {
-         return <LoginForm />
-    }
-    return(
+const projectID = 'bb09b46d-a654-48d3-ba32-629c976a3b22';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
     <ChatEngine
-     height="100vh"
-     projectID="bb09b46d-a654-48d3-ba32-629c976a3b22"
-     userName={localStorage.getItem('username')}
-     userSecret={localStorage.getItem('password')}
-     renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-    );
-}
+  );
+};
 
-
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,61 +1,60 @@
 import React from 'react';
-import {useState} from 'react';
-import {sendMessage,isTyping} from 'react-chat-engine';
-import {SendOutlined,PictureOutlined} from '@ant-design/icons';
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
+const MessageForm = (props) => {
+  const [value, setValue] = useState('');
+  const { chatId, creds } = props;
 
-const MessageForm =(props)=>
-{
-    const [value,setValue]=useState('');
-    const {chatId,creds}=props;
+  const handleChange = (event) => {
+    setValue(event.target.value);
 
-    const handleSubmit=(event)=>
-{
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    const text=value.trim();
-    if(text.length>0) sendMessage(creds,chatId,{text});
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
+
     setValue('');
+  };
 
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: '' });
+  };
 
-}
-const handleChange=(event)=>
-{
-    setValue(event.target.value);
-    isTyping(props,chatId);
-
-}
-
-const handleUpload=(event)=>
-{
-  sendMessage(creds.chatId,{files:event.target.files,text:''})
-}
-    return (
-        <form className="message-form" onSubmit={handleSubmit}> 
-        <input 
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
         className="message-input"
-        placeholder="Send a message ..."
+        placeholder="Send a message..."
         value={value}
         onChange={handleChange}
         onSubmit={handleSubmit}
-        />
-        <label htmlFor="upload-button">
-            <span className="image-button"></span>
-            <PictureOutlined className="picture-icon" />
-        </label>
-        <input 
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
         type="file"
         multiple={false}
         id="upload-button"
-        style={{display:'none'}}
-        onChange={handleUpload}
-        />
-        <button type="submit" className="send-button">
-            <SendOutlined className="send-icon" /> 
-        </button>
-        </form>
-    );
-
-    }
+        style={{ display: 'none' }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+    </form>
+  );
+};
 
 export default MessageForm;
